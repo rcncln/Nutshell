ff61bb2185c5e6ce25c407a232b7461189263d9e
rcncln <53015856+rcncln@users.noreply.github.com>
2024-05-23 10:57:15 +0200

    initial files

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post('/nutshell-webhook', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
